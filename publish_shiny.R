b95f3d6fe2809413747c97f4e5b0f240667789a3
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2024-06-14 11:16:48 +0200

    publishing functions moved to project.aid

--- publish_shiny.R
+++ publish_shiny.R
@@ -1,44 +1,28 @@
 # pak::pak("agdamsbo/project.aid")
 
-merge_scripts <- function(path,files,strip.roxygen=TRUE){
-sink(path)
+# merge_scripts(path=here::here("app/functions.R"),files=list.files("R/",pattern = ".R$",full.names = TRUE))
 
-for(i in seq_along(files)){
-  current_file <-  readLines(files[i])
+# styler::style_file("app/functions.R")
 
-  if (strip.roxygen){
-  current_file <- sub("(?m)^\\#\\'.*\n?", "", current_file, perl=T)
-  }
+project.aid::merge_scripts(
+  files = c(
+    list.files("R/", pattern = ".R$", full.names = TRUE),
+    "app/server_raw.R"
+  ),
+  dest = here::here("app/server.R")
+)
 
-  cat("\n\n########\n#### Current file:",files[i],"\n########\n\n")
-  cat(current_file, sep ="\n")
-}
-sink()
-}
-
-deploy_shiny <- function (path=here::here("app/"), files=NULL, account.name, name.app,
-          name.token, name.secret){
-  rsconnect::setAccountInfo(name = account.name, token = keyring::key_get(service = name.token),
-                            secret = keyring::key_get(service = name.secret))
-  rsconnect::deployApp(appDir = path,appFiles = files, lint = TRUE, appName = name.app)
-}
-
-merge_scripts(path=here::here("app/functions.R"),files=list.files("R/",pattern = ".R$",full.names = TRUE))
-
-styler::style_file("app/functions.R")
-
-merge_scripts(path=here::here("app/server.R"),files=c("app/functions.R","app/server_raw.R"))
-
-deploy_shiny(
-  files= c("server.R","ui.R"),
+project.aid::deploy_shiny(
+  files = c("server.R", "ui.R"),
   account.name = "agdamsbo",
   name.app = "banko",
   name.token = "rsconnect_agdamsbo_token",
   name.secret = "rsconnect_agdamsbo_secret"
 )
 
+# It also runs as shinylive app, but quite slow. But it runs!
+# Will implement in vignette.
+
 # shinylive::export(appdir = "app", destdir = "docs")
 #
 # httpuv::runStaticServer(dir = "docs")
-
-
