f12cd154c3c4a9a1240bb2ed0d88e56480d33e9f
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2025-08-29 10:38:40 +0200

    implement bingo and memory cards

--- R/cards.R
+++ R/cards.R
@@ -55,10 +55,12 @@ generate <- function() {
 #' @export
 #'
 #' @examples
-#' cards(5) |> purrr::map(\(.x) attr(.x,which = "banko_seed")) |> unique()
-#' attr(cards(5),which = "banko_seed" )
-cards <- function(n,seed=NULL) {
-  if (is.null(seed)) seed <- abs(sample(.Random.seed,1))
+#' cards(5) |>
+#'   purrr::map(\(.x) attr(.x, which = "banko_seed")) |>
+#'   unique()
+#' attr(cards(5), which = "banko_seed")
+cards <- function(n, seed = NULL) {
+  if (is.null(seed)) seed <- abs(sample(.Random.seed, 1))
 
   set.seed(seed)
 
@@ -67,7 +69,7 @@ cards <- function(n,seed=NULL) {
   # Repeats until n unique in list
   repeat{
     # Generates new card
-    p <- structure(generate(),banko_seed=seed)
+    p <- structure(generate(), banko_seed = seed)
 
     # Tests if unique compared to rest in list before appending
     if (is_unique_card(p, l)) {
@@ -80,8 +82,10 @@ cards <- function(n,seed=NULL) {
     }
   }
   # outputs unique cards
-  structure(l,banko_seed=seed,
-            class=c("banko_list",class(l)))
+  structure(l,
+    banko_seed = seed,
+    class = c("banko_list", class(l))
+  )
 }
 
 
@@ -159,6 +163,102 @@ unique_numbers <- function(cards) {
 #'
 #' @examples
 #' 1:10 |> shuffle_seq()
-shuffle_seq <- function(data){
+shuffle_seq <- function(data) {
   sample(data, size = length(data), replace = FALSE)
+}
+
+generate_bingo <- function(dim = c(2, 3), base = 1:90, sort = FALSE) {
+  out <- sample(base, prod(dim))
+  if (isTRUE(sort)) {
+    out <- sort(out)
+  }
+  structure(matrix(out, nrow = dim[1]), class = c("banko", class(out)))
+}
+
+#' Create unique bingo cards with specified dimensions and custom base
+#'
+#' @param n
+#' @param dim
+#' @param base
+#' @param sort
+#' @param seed
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' bingo(2, base = 1:15, sort = TRUE)
+#' bingo(2, base = list.files("/Users/au301842/Library/CloudStorage/OneDrive-Personal/Research/PhD/Formidling/Pictograms", full.names = TRUE))
+bingo <- function(n, dim = c(2, 3), base = 1:90, sort = FALSE, seed = NULL) {
+  if (is.null(seed)) seed <- abs(sample(.Random.seed, 1))
+
+  set.seed(seed)
+  # browser()
+
+  l <- list()
+
+  stopifnot(length(base) / prod(dim) > 2)
+
+  # Repeats until n unique in list
+  repeat{
+    # Generates new card
+    p <- structure(generate_bingo(dim = dim, base = base, sort = sort), banko_seed = seed)
+
+    # Tests if unique compared to rest in list before appending
+    if (is_unique_card(p, l)) {
+      l[[length(l) + 1]] <- p
+    }
+
+    # Breaks when l has length n
+    if ((length(l) == n)) {
+      break
+    }
+  }
+  # outputs unique cards
+  structure(l,
+    banko_seed = seed,
+    class = c("banko_list", class(l))
+  )
+}
+
+#' Title
+#'
+#' @param n
+#' @param dim
+#' @param base
+#' @param sort
+#' @param seed
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' memory(n = 2)
+#' memory(base = list.files("/Users/au301842/Library/CloudStorage/OneDrive-Personal/Research/PhD/Formidling/Pictograms", full.names = TRUE))
+memory <- function(n = NULL, dim = c(2, 3), base = 1:90, sort = FALSE, seed = NULL) {
+  if (is.null(seed)) seed <- abs(sample(.Random.seed, 1))
+
+  set.seed(seed)
+  ## Splitting in groups
+  out <- chunks_of_n(shuffle_seq(base), n = prod(dim), even = FALSE)
+  ## Removing last if not to size
+  out <- out[lapply(out, length) == prod(dim)]
+
+  ## Applying dims and class
+  out <- lapply(out, \(.x){
+    structure(matrix(.x, nrow = dim[1]), class = c("banko", class(.x)))
+  })
+
+  if (isTRUE(sort)) {
+    out <- lapply(out, sort)
+  }
+
+  if (!is.null(n)) {
+    out <- out[seq_len(n)]
+  }
+
+  structure(out,
+    banko_seed = seed,
+    class = c("banko_list", class(out))
+  )
 }
